8a9aeb79a9c28320614d96e3ea6ab4fa8751bdd1
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-24 14:57:17 -0300

    Feat: path of specifications routes defined

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationRoutes } from './routes/specifications.routes';
 
 const app = express();
 
 app.use(express.json())
 
 app.use("/categories",categoriesRoutes)
+
+app.use("/specifications", specificationRoutes)
 
 app.listen(3030, ()=> console.log("Server is running!"));
 
