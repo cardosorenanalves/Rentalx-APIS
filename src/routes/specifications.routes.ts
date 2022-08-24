53034580c8b6bb8e57705d6f018fd9b96773277d
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-24 14:56:48 -0300

    Feat: specification route post create

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { SpecificationRepository } from "../Modules/Cars/repositories/SpecificationRepository";
+import { CreateSpecificationService } from "../Modules/Cars/services/CreateSpecificationService";
+
+
+
+const specificationRoutes = Router()
+
+const specificationRepository = new SpecificationRepository();
+
+specificationRoutes.post('/', (request, response) => {
+    const {name, description} = request.body;
+    const createSpecificationService = new CreateSpecificationService(specificationRepository);
+
+    createSpecificationService.execute({name, description});
+
+    return response.status(201).send()
+})
+
+export { specificationRoutes }
