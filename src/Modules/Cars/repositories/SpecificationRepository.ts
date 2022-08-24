c758cdf1f00aa8e05ec0c98d29689011c2becda9
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-24 14:56:20 -0300

    Feat: Specification Repository created

---
+++ src/Modules/Cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from "../model/Specification";
+import { ICreateSpecificationDTO, ISpecificationsRepository } from "./ISpecificationsRepository";
+
+
+
+class SpecificationRepository implements ISpecificationsRepository{
+    private specification: Specification[];
+
+    constructor(){
+        this.specification = [];
+    }
+   
+    create({ description, name }: ICreateSpecificationDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification,{
+            name,
+            description,
+            created_at: new Date()
+        })
+
+        this.specification.push
+    };
+
+    findByName(name: string): Specification {
+        const specification = this.specification.find(
+            (specification) => specification.name === name
+            );
+            return specification;
+    }
+
+
+}
+
+export { SpecificationRepository }
