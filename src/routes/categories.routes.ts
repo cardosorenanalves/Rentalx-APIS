c104bf8f0fe05a7c0da21b3b5586bd4c8f183a2b
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-25 10:20:45 -0300

    Refactor: controllers implementations

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,15 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../Modules/Cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../Modules/Cars/services/CreateCategoryService";
+import { createCategoryController } from "../Modules/Cars/useCases/createCategory";
+import { listCategoriesController } from "../Modules/Cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRespository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const {name, description} = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRespository);
-
-    createCategoryService.execute({name, description});
-
-    return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-    const all = categoriesRespository.list();
-
-    return response.json(all)
+ return listCategoriesController.handle(request, response);
 })
 
 export {categoriesRoutes};
