d00abfa37e5565b8524534609b60740e84c1777d
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-26 09:58:01 -0300

    Feat: create ImportCategoryUseCase

---
+++ src/Modules/Cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,20 @@
+import { parse as csvParse } from "csv-parse";
+
+import fs from "fs";
+
+class ImportCategoryUseCase {
+
+    execute(file: Express.Multer.File): void{
+        console.log(file)
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on('data', async (line) => {
+
+        })
+    }
+}
+export { ImportCategoryUseCase }
