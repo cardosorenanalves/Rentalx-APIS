89ed5aaab41b5c69659452d03fe01d3289d5be61
Renan Cardoso <renancardoso0307@gmail.com>
2022-08-26 09:59:21 -0300

    Refactor: Multer and route post '/import' implanted

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../Modules/Cars/useCases/createCategory";
+import { importCategoryController } from "../Modules/Cars/useCases/importCategory";
 import { listCategoriesController } from "../Modules/Cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: './tmp'
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +16,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
  return listCategoriesController.handle(request, response);
-})
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+   return importCategoryController.handle(request, response);
+  });
 
 export {categoriesRoutes};
